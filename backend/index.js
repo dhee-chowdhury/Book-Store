f3ea7d374da91e35394b8d9f2cd87280f78d7a65
dhee-chowdhury <dhee.chowdhury@gmail.com>
2023-11-13 12:20:25 +0600

    added get all book route

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,9 @@ import { Book } from "./models/bookModel.js";
 
 const app = express();
 
+// middleware for parsing request body
+app.use(express.json());
+
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("welcome to MERN Stack tutorial");
@@ -14,13 +17,14 @@ app.get("/", (req, res) => {
 // Route to save a new book
 app.post("/books", async (req, res) => {
   try {
+    // checking if the fields are empty
     if (!req.body.title || !req.body.author || !req.body.publishYear) {
-      return res
-        .status(400)
-        .send({
-          message: "Send all required fields: title, author, publishYear",
-        });
+      return res.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
     }
+
+    // creating a new book
 
     const newBook = {
       title: req.body.title,
@@ -28,6 +32,20 @@ app.post("/books", async (req, res) => {
       publishYear: req.body.publishYear,
     };
     const book = await Book.create(newBook);
+    return res.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
   } catch (error) {
     console.log(error.message);
     res.status(500).send({ message: error.message });
