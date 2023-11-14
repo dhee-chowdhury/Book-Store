77fa36ef3d1b81c8e553ea9b6540f3c591bed38a
dhee-chowdhury <dhee.chowdhury@gmail.com>
2023-11-14 16:58:00 +0600

    edit book done

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,7 +1,91 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+
 const EditBook = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:5000/books/${id}`)
+      .then((res) => {
+        setAuthor(res.data.author);
+        setTitle(res.data.title);
+        setPublishYear(res.data.publishYear);
+        setLoading(false);
+      })
+      .catch((error) => {
+        alert("Something went wrong.");
+        setLoading(false);
+        console.log(error);
+      });
+  }, [id]);
+
+  const handleEditBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios
+      .put(`http://localhost:5000/books/${id}`, data)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert("Something went wrong. try again");
+        console.log(error);
+      });
+  };
+
   return (
-    <div>
-      <h2>This for editing a book</h2>
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Add a book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col border-2 border-sky-500 rounded-xl w-[800px] p-4 mx-auto">
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Author</label>
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Publish year</label>
+          <input
+            type="number"
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <button onClick={handleEditBook} className="p-2 bg-sky-500 m-8">
+          Add
+        </button>
+      </div>
     </div>
   );
 };
